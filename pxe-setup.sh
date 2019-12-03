f10566110d721eec54760953aa30a64c113f1408
lexandr0s <lexandr0s@users.noreply.github.com>
2019-12-03 06:47:03 +0400

    Update pxe-setup.sh

--- pxe-setup.sh
+++ pxe-setup.sh
@@ -57,6 +57,7 @@ fi
 
 cat /tmp/pxe-server/hiveos-pxe-diskless-master/pxeserver/hiveramfs/x* > /tmp/pxe-server/hiveos-pxe-diskless-master/pxeserver/hiveramfs/hiveramfs.tar.xz
 rm /tmp/pxe-server/hiveos-pxe-diskless-master/pxeserver/hiveramfs/x*
+[[ -f ${dir}/pxeserver/server.conf ]] && cp ${dir}/pxeserver/server.conf /tmp/pxe-server/hiveos-pxe-diskless-master/pxeserver
 
 echo
 echo -e "${GREEN}> Copy PXE-server package to destination directory.${NOCOLOR}"
